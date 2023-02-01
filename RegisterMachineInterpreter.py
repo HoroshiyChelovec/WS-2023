60806107ed8c5e1e168822d1d0cf2e0392242187
unknown <mathuser@K24.179.lan>
2023-02-01 21:34:11 +0300

    Добавил комманду SET

--- RegisterMachineInterpreter.py
+++ RegisterMachineInterpreter.py
@@ -1,11 +1,25 @@
 class RegisterMachineInterpreter:
-    registers: dict[str: str] = {}
+    registers = {}
+
+    def set(self, registers):
+        r1, r2 = registers.split(' ')
+        if r2.isnumeric():
+            data = int(r2)
+        else:
+            data = int(self.registers[r2], 2)
+        self.registers[r1] = ''
+        for i in range(8):
+            self.registers[r1] = str(data % 2) + self.registers[r1]
+            data = data >> 1
+        print(self.registers[r1])
 
     def execute(self, command: str):
-        pass
+        operator = command[0]
+        if command[0] == '&':
+            self.set(command[2:])
 
     def interpret(self, code):
-        keep_going: bool = True
-        command: int = 0
+        keep_going = True
+        command = 0
         while keep_going:
-            self.execute(list[command])
+            self.execute(code[command])
