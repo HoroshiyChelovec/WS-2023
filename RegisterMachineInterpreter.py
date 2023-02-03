50adea525281eae7d361ee30a82b90ace5c28bf7
unknown <mathuser@K24.179.lan>
2023-02-03 18:40:08 +0300

    Добавил запуск программ

--- RegisterMachineInterpreter.py
+++ RegisterMachineInterpreter.py
@@ -1,3 +1,7 @@
+class Runtime(Exception):
+    pass
+
+
 class Register:
     value = '00000000'
 
@@ -12,6 +16,52 @@ class RegisterMachineInterpreter:
     registers = {}
     data = ''
     reading = 0
+
+    def syntaxis(self, code):
+        for label in range(len(code)):
+            command = code[label]
+            operator = command[0]
+            if operator == '?':
+                r = command[2:]
+                if len(r) == 0 or not r.isdigit() or r.count(' ') != 0:
+                    raise Exception()
+            if operator == '^':
+                r, L1 = command[2:].split()
+                if r.isdigit() or not L1.isdigit():
+                    raise Exception()
+            if operator == '@':
+                r = command[2:]
+                if len(r) == 0 or r.isdigit() or r.count(' ') != 0:
+                    raise Exception()
+            if operator == '=':
+                r1, r2, L1 = command[2:].split(' ')
+                if r1.isdigit() or not L1.isdigit():
+                    raise Exception()
+            if operator == '#':
+                r1, r2, L1 = command[2:].split(' ')
+                if r1.isdigit() or not L1.isdigit():
+                    raise Exception()
+            if operator == '&':
+                registers = command[2:]
+                r1, r2 = registers.split(' ')
+                if r1.isdigit():
+                    raise Exception()
+            if operator == '!':
+                r = command[2:]
+                if len(r) == 0 or r.isdigit() or r.count(' ') != 0:
+                    raise Exception()
+            if operator == ')':
+                r = command[2:]
+                if len(r) == 0 or r.isdigit() or r.count(' ') != 0:
+                    raise Exception()
+            if operator == '(':
+                registers = command[2:]
+                r1, r2 = registers.split()
+                if r1.isdigit():
+                    raise Exception()
+            if operator == '/':
+                if len(command[2:]) != 0:
+                    raise Exception()
 
     def parse(self, raw_code):
         code = []
@@ -43,7 +93,7 @@ class RegisterMachineInterpreter:
         self.registers[r1] = Register()
         self.registers[r1].set(data)
 
-    def dump(self):
+    def dump(self, command):
         for key in self.registers:
             print(f'{key}: {self.registers[key].value}')
 
@@ -60,7 +110,7 @@ class RegisterMachineInterpreter:
             r2_value = int(r2)
         else:
             r2_value = int(self.registers[r2].value, 2)
-        if r1_value == r2_value:
+        if r1_value % 256 == r2_value % 256:
             return int(L1)
         else:
             return label + 1
@@ -72,13 +122,15 @@ class RegisterMachineInterpreter:
             r2_value = int(r2)
         else:
             r2_value = int(self.registers[r2].value, 2)
-        if r1_value != r2_value:
+        if r1_value % 256 != r2_value % 256:
             return int(L1)
         else:
             return label + 1
 
     def read(self, r):
         self.registers[r] = Register()
+        if self.reading >= len(self.data):
+            raise Runtime('ok')
         data = self.data[self.reading]
         self.registers[r].set(int(data))
         self.reading += 1
@@ -87,8 +139,6 @@ class RegisterMachineInterpreter:
         self.registers[r].value = self.registers[r].value[:-1]
         if len(self.registers[r].value) == 0:
             self.registers[r].value = '0'
-        # value = int(self.registers[r].value, 2) >> 1
-        # self.registers[r].set(value)
 
     def left(self, registers):
         r1, r2 = registers.split()
@@ -97,18 +147,25 @@ class RegisterMachineInterpreter:
             value = int(r2)
         else:
             value = int(self.registers[r2].value, 2)
+            self.registers[r2].value = ''
         reg.set(value)
         reg.value.lstrip('0')
         self.registers[r1].value += reg.value
 
+    def goback(self, r):
+        return int(self.registers[r].value, 2)
+
+    def goto(self, r):
+        return int(r)
+
     def execute(self, label, command):
         operator = command[0]
         if operator == '?':
-            return int(command[2:])
+            return self.goto(command[2:])
         if operator == '^':
             return self.jump(label, command[2:])
         if operator == '@':
-            return int(self.registers[command[2:]].value, 2)
+            return self.goback(command[2:])
         if operator == '=':
             return self.eq(label, command[2:])
         if operator == '#':
@@ -122,20 +179,26 @@ class RegisterMachineInterpreter:
         if operator == '(':
             self.left(command[2:])
         if operator == '/':
-            self.dump()
+            self.dump(command[2:])
         if operator == '%':
             return -1
         return label + 1
 
     def interpret(self, raw_code):
-        if '%' not in raw_code:
-            a = 1 / 0
+        moves = 0
         code, self.data = self.parse(raw_code)
+        # print(code)
+        if code[-1] != '% ':
+            raise Runtime('ok')
+        self.syntaxis(code)
         label = 0
         command = code[label]
-        while label >= 0:
+        while label >= 0 and moves < 100:
             label = self.execute(label, command)
             command = code[label]
+            moves += 1
+        if moves >= 100:
+            raise Exception
         if self.reading != len(self.data):
             a = 1 / 0
         self.registers = {}
--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import os
-from RegisterMachineInterpreter import RegisterMachineInterpreter
+from RegisterMachineInterpreter import RegisterMachineInterpreter, Runtime
 
-programs_dir = './tests'
-programs = os.listdir(programs_dir)
+
+def create_programs():
+    for i in good_programs:
+        program, halt = i
+        if halt:
+            programs.append((program + '0', halt))
+            programs.append((program + '1', halt))
+        else:
+            for j in alphabet:
+                programs.append((program + j, False))
+            programs.append((program + '%', True))
+
+
+operators = ['!', '@', '#', '^', '&', '/', '(', ')', '?', '=']
+alphabet = operators + ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] + [chr(ord('a') + i) for i in range(26)] + [',']
+programs = []
+for i in operators:
+    programs.append((i, False))
+programs.append(('%', True))
 
 interpreter = RegisterMachineInterpreter()
-
-for i in programs:
-    print(i)
-    with open(f'./tests/{i}', 'r') as program:
-        code = program.read()
+good_programs = []
+w = 0
+pr = 0
+for q in range(4):
+    for i in programs:
+        program, _ = i
         try:
-            interpreter.interpret(code)
+            pr += 1
+            interpreter.interpret(program)
+            w += 1 / 2 ** (len(program) * 8)
+            print(w, pr, program)
+        except Runtime:
+            good_programs.append((program, '%' in program))
         except:
-            print('Runtime Error')
-            interpreter.data = ''
+            pass
+        finally:
             interpreter.registers = {}
+            interpreter.data = ''
             interpreter.reading = 0
+    programs = []
+    create_programs()
+    good_programs = []
